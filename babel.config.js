1690ef70f6a0382cb8949d7c6baaaa4a43a40936
张元 <zhangyuan@ctsi.com.cn>
2020-08-03 09:49:20 +0800

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', {
+      modules: false
+    }]
+  ],
+  plugins: [
+    ['component', {
+      libraryName: 'mint-ui',
+      style: true
+    }]
   ]
 }
