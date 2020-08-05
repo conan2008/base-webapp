c0351feedd20c3fc5c7535d2cab471a573b1403e
张元 <zhangyuan@ctsi.com.cn>
2020-08-05 17:23:28 +0800

    test 7

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home1</router-link>|
+      <router-link to="/">Home12</router-link>|
       <router-link to="/about">About2</router-link>
     </div>
     <router-view />
