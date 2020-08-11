b2378973f605ea406af7b4d1677ea23ea596a578
张元 <zhangyuan@ctsi.com.cn>
2020-08-11 12:06:45 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home1234567</router-link>|
+      <router-link to="/">Home12345678</router-link>|
       <router-link to="/about">About2</router-link>
     </div>
     <router-view />
