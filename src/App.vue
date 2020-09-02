9626e78ca98ffff9623c97140b91bed248603134
conan2008 <200832342@qq.com>
2020-09-02 18:38:33 +0800

    jssdk

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">ZBC 812</router-link>|
+      <router-link to="/">ZBC 8121</router-link>|
       <router-link to="/about">About212</router-link>
     </div>
     <router-view />
