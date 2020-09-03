9bc94519d333a9fd87817e99e2d01ca20bd51e3f
conan2008 <200832342@qq.com>
2020-09-03 17:18:30 +0800

    wx

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,14 @@ export default {
       console.log('sss', wx.chooseImage)
     }, 1000)
 
-    wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: 'wxc0688c5c8b980317', // 必填，公众号的唯一标识
-      timestamp: '123', // 必填，生成签名的时间戳
-      nonceStr: '123', // 必填，生成签名的随机串
-      signature: 'conan2008', // 必填，签名
-      jsApiList: ['chooseImage'] // 必填，需要使用的JS接口列表
-    })
+    // wx.config({
+    //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    //   appId: 'wxc0688c5c8b980317', // 必填，公众号的唯一标识
+    //   timestamp: '', // 必填，生成签名的时间戳
+    //   nonceStr: '', // 必填，生成签名的随机串
+    //   signature: '', // 必填，签名
+    //   jsApiList: ['chooseImage'] // 必填，需要使用的JS接口列表
+    // })
   },
   methods: {
     handleClick () {
