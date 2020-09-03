7cc42178277f044c45751151cc503464cb0ed4ca
conan2008 <200832342@qq.com>
2020-09-03 16:06:38 +0800

    wx

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,27 @@ export default {
       console.log(window.navigator.userAgent)
       console.log('sss', wx.chooseImage)
     }, 1000)
+
+    wx.config({
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: '', // 必填，公众号的唯一标识
+      timestamp: '', // 必填，生成签名的时间戳
+      nonceStr: '', // 必填，生成签名的随机串
+      signature: '', // 必填，签名
+      jsApiList: [] // 必填，需要使用的JS接口列表
+    })
   },
   methods: {
     handleClick () {
-      window.wx.chooseImage({
-        count: 2, // 默认9
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function (res) {
-          console.log(res)
-        }
+      wx.ready(function () {
+        wx.chooseImage({
+          count: 2, // 默认9
+          sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+          sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+          success: function (res) {
+            console.log(res)
+          }
+        })
       })
     }
   }
